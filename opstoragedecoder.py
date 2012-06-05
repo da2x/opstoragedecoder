f02803292d9251e2bc7ac6c3ba158ea0329c5bf8
Daniel Aleksandersen <code@daniel.priv.no>
2012-06-06 01:22:06 +0200

    First implementation
    Complete with usage instructions. :-)

---
+++ opstoragedecoder.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from xml.dom.minidom import *
+from base64 import *
+import sys
+
+def pad(string, length=50, padchar=" "):
+	return string + padchar * (length-len(string))
+
+def getDecodedElementTextNode(entry, elem):
+	try:
+		value = entry.getElementsByTagName(elem)
+		value = value[0].childNodes[0].data
+		return b64decode(value).decode("UTF-16")
+	except:
+		return "nil" # represents literal value
+
+if not len(sys.argv) == 2:
+ 	print "Prints a human-readable presentation of the encoded contents in a permanent storage (pstorage) database file used by Opera."
+	print "\tUsage: `./opstoragedecoder.py <file>`"
+	sys.exit(1)
+
+document = sys.argv[1]
+entries = parse(document).getElementsByTagName("e")
+data = { getDecodedElementTextNode(entry, "k") : getDecodedElementTextNode(entry, "v") for entry in entries }
+
+for key,value in data.items():
+	print pad(key) + value
